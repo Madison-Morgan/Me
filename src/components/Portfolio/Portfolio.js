e117046741c0a8231640568b4e1a66c55e913aa1
mmorg031 <madison.morgan.eng@gmail.com>
2021-10-15 06:42:53 -0400

    Adding under construction to Portfolio section until I find time to fix up and add demos

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -18,7 +18,7 @@ class Portfolio extends Component {
     }
     toggleClass(project) {
         const currentState = this.state[project];
-        this.setState({ one: !currentState });
+        this.setState({ [project]: !currentState });
         console.log(this.state.one);
     };
 
@@ -29,7 +29,7 @@ class Portfolio extends Component {
                     <div className="portfolio-header">
                         <span className="portfolio-title">Portfolio 💻</span>
                         <div className="portfolio-content">
-                            <p>Click on a project to learn more!</p>
+                            <p>Click on a project to learn more! (Under Construction 👷‍♀️)</p>
                         </div>
                     </div>
                 </Fade>
@@ -41,12 +41,14 @@ class Portfolio extends Component {
                         <Fade left>
                             <div className="portfolio-proj1"
                                 onClick={() => this.toggleClass('one')}>
-                                <FontAwesomeIcon icon={faTimes} 
-                                style={{ fontSize: 25, color: "white"
-                                    /*position: "absolute", right: 15, top: 15*/}}
+                                <FontAwesomeIcon icon={faTimes}
+                                    style={{
+                                        fontSize: 25, color: "white"
+                                        /*position: "absolute", right: 15, top: 15*/
+                                    }}
                                     onClick={() => this.toggleClass('one')} />
                                 <Project
-                                    activeState={this.state.one ? 'active-project': 'unactive'}
+                                    activeState={this.state.one ? 'active-project' : 'unactive'}
                                     header={'Cannasplain'}
                                     text={'A real-time React retail product display that uses MongoDB database to keep track of and display strains and quantities for a local Cannabis shop.'}
                                     tech={['React', 'MongoDB', 'Express.Js', 'Node.js']}
@@ -58,8 +60,17 @@ class Portfolio extends Component {
 
 
                         <Fade right>
-                            <div className="portfolio-proj2">
-                                <Project header={'Shoot'}
+                            <div className="portfolio-proj2"
+                                onClick={() => this.toggleClass('two')}>
+                                <FontAwesomeIcon icon={faTimes}
+                                    style={{
+                                        fontSize: 25, color: "white"
+                                        /*position: "absolute", right: 15, top: 15*/
+                                    }}
+                                    onClick={() => this.toggleClass('two')} />
+                                <Project
+                                    activeState={this.state.two ? 'active-project' : 'unactive'}
+                                    header={'Shoot'}
                                     text={'An interactive computer vision application that you can challenge against Rock-Paper-Scissors.'}
                                     tech={['Tensorflow', 'OpenCV', 'React', 'Pandas']}
                                     liveLink={''}
@@ -67,19 +78,41 @@ class Portfolio extends Component {
                             </div>
                         </Fade>
                     </div>
+
+
                     <div className="portfolio-row">
                         <Fade left>
-                            <div className="portfolio-proj2">
-                                <Project header={'Witchy-Witch'}
+                            <div className="portfolio-proj2"
+                                onClick={() => this.toggleClass('three')}>
+                                <FontAwesomeIcon icon={faTimes}
+                                    style={{
+                                        fontSize: 25, color: "white"
+                                        /*position: "absolute", right: 15, top: 15*/
+                                    }}
+                                    onClick={() => this.toggleClass('three')} />
+                                <Project
+                                    activeState={this.state.three ? 'active-project' : 'unactive'}
+                                    header={'Witchy-Witch'}
                                     text={'An random animated and interactive witch, using React.'}
                                     tech={['React', 'Hooks']}
                                     liveLink={''}
                                     githubLink={''} />
                             </div>
                         </Fade>
+
+
                         <Fade right>
-                            <div className="portfolio-proj1">
-                                <Project header={'Gather'}
+                            <div className="portfolio-proj1"
+                                onClick={() => this.toggleClass('four')}>
+                                <FontAwesomeIcon icon={faTimes}
+                                    style={{
+                                        fontSize: 25, color: "white"
+                                        /*position: "absolute", right: 15, top: 15*/
+                                    }}
+                                    onClick={() => this.toggleClass('four')} />
+                                <Project
+                                    activeState={this.state.four ? 'active-project' : 'unactive'}
+                                    header={'Gather'}
                                     text={'An Android application to help friends plan a meetup, with everyones busy scedhule.'}
                                     tech={['Firebase', 'Kotlin', 'Android Studio']}
                                     liveLink={''}
